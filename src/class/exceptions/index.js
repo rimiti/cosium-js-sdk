9af805d9840cf58e5c1676183ca0cd823bb502c0
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-21 15:10:43 +0200

    feat(exceptions/index.js) ConfigurationWrongFormat exception exported

--- src/class/exceptions/index.js
+++ src/class/exceptions/index.js
@@ -1,7 +1,9 @@
 import AuthenticationBadRequest from './authentication/bad_request'
 import ConfigurationWrongCredentials from './configuration/wrong_credentials'
+import ConfigurationWrongFormat from './configuration/wrong_format'
 
 export {
   AuthenticationBadRequest,
+  ConfigurationWrongFormat,
   ConfigurationWrongCredentials
 }
