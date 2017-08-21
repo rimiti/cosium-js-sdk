3ca2bc0096ff7fc19cee153bedd8ed2af712d510
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-21 16:07:37 +0200

    chore(test/index.js) instantiate sdk before be used in all tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,14 @@
 import test from 'ava'
 import sdk from '../src/lib'
+import SDK from '../src/class/sdk'
+
+let instance = {}
+
+test.before(t => {
+  sdk.configure({credentials: {username: "username", password: "password"}})
+  instance = sdk.create()
+  t.is(instance instanceof SDK, true)
+})
 
 test('Getting available time slots', t => {
   t.is(true, true)
