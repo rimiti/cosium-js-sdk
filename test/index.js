1e6f922e4e27e2d3c2943b200e413dff473feb30
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-21 14:25:18 +0200

    chore(test/index.js) tests functions created

---
+++ test/index.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import sdk from '../src/lib'
+
+test('Getting available time slots', t => {
+  t.is(true, true)
+})
+
+test('Create appointment', t => {
+  t.is(true, true)
+})
+
+test('Cancel appointment', t => {
+  t.is(true, true)
+})
