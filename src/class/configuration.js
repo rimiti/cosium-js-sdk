5c6629f4567e604d3757396fd1ee893491273124
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-21 15:20:48 +0200

    feat(class/configuration.js) configuration class created

---
+++ src/class/configuration.js
@@ -0,0 +1,52 @@
+import {ConfigurationWrongFormat} from './exceptions'
+import Authentication from './authentication'
+
+export default class Configuration {
+
+  /**
+   * @description Auto-hydrate object from configuration
+   * @param config
+   */
+  constructor(config) {
+    this._hydrate(config, this._itemsToHydrate())
+  }
+
+  get format() {
+    return this._format
+  }
+
+  set format(value) {
+    if (value !== 'json') throw new ConfigurationWrongFormat()
+    this._format = value
+  }
+
+  get credentials() {
+    return this._credentials
+  }
+
+  set credentials(value) {
+    this._credentials = new Authentication(value)
+  }
+
+  /**
+   * @description Hydrate object from configuration settings
+   * @param obj
+   * @param attributes
+   * @private
+   */
+  _hydrate(obj, attributes) {
+    if (!obj) return
+    for (let item of attributes) {
+      this[item] = (obj[item]) ? obj[item] : ''
+    }
+  }
+
+  /**
+   * @description Return array with items names to hydrate
+   * @return {[string,string]}
+   * @private
+   */
+  _itemsToHydrate() {
+    return ['format', 'credentials']
+  }
+}
