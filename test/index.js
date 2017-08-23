f8f92eaebed233fe88ddcafbb0f3de9e7c0a4424
Dimitri <contact@dimsolution.com>
2017-08-23 10:05:09 +0200

    chore(test/index.js) test to retrieve available time slots

--- test/index.js
+++ test/index.js
@@ -11,7 +11,22 @@ test.before(t => {
 })
 
 test('Get available time slots', t => {
-  t.is(true, true)
+
+  let params = {
+    siteCode: "c1",
+    startDate: "2017-09-23T12:00:00.000Z",
+    endDate: "2017-09-23T13:00:00.000Z"
+  }
+
+  let expected = [
+    { date: '2017-09-23T12:00:00.000+0000', qualifications: [ 'CONTACT_LENS', 'OPTIC', 'HEARING_AID' ] },
+    { date: '2017-09-23T12:30:00.000+0000', qualifications: [ 'CONTACT_LENS', 'OPTIC', 'HEARING_AID' ] }
+  ]
+
+  return instance.getAvailableTimeslots(params).then((resp) => {
+    t.deepEqual(resp, expected)
+  })
+
 })
 
 test('Create appointment', t => {
