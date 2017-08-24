9924adbd5ad597d5a7791a1e98eb4ad6c414803d
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-24 12:09:03 +0200

    feat(class/exceptions/api/wrong_datetime_values.js) new exception created

--- src/class/exceptions/index.js
+++ src/class/exceptions/index.js
@@ -2,6 +2,7 @@ import AuthenticationBadRequest from './authentication/bad_request'
 import ConfigurationWrongCredentials from './configuration/wrong_credentials'
 import ConfigurationWrongFormat from './configuration/wrong_format'
 import MissingMandatoryParameter from './api/missing_mandatory_parameter'
+import WrongDatetimeValues from './api/wrong_datetime_values'
 import WrongDatetimes from './api/wrong_datetimes'
 
 export {
@@ -9,5 +10,6 @@ export {
   ConfigurationWrongFormat,
   ConfigurationWrongCredentials,
   MissingMandatoryParameter,
+  WrongDatetimeValues,
   WrongDatetimes
 }
