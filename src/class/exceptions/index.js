6a21984ecae3e53db43e25c7ae160268c57e7ed7
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-24 14:59:42 +0200

    feat(class/exceptions/api/unknown_error.js) unknownError exception created

--- src/class/exceptions/index.js
+++ src/class/exceptions/index.js
@@ -5,6 +5,7 @@ import MissingMandatoryParameter from './api/missing_mandatory_parameter'
 import WrongDatetimeValues from './api/wrong_datetime_values'
 import WrongDatetimes from './api/wrong_datetimes'
 import UnknownCategoryCode from './api/unknown_category_code'
+import UnknownError from './api/unknow_error'
 import UnavailableSlot from './api/unavailable_slot'
 import BookingNotFound from './api/booking_not_found'
 
@@ -16,6 +17,8 @@ export {
   WrongDatetimeValues,
   WrongDatetimes,
   UnknownCategoryCode,
+  UnavailableSlot,
+  UnknownError
   UnavailableSlot,
   BookingNotFound
 }
