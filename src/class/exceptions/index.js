beafae12866fdab976d1537de8cf0b4ddd3578ac
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-24 11:54:03 +0200

    feat(exception/index.js) exception exported

--- src/class/exceptions/index.js
+++ src/class/exceptions/index.js
@@ -2,10 +2,12 @@ import AuthenticationBadRequest from './authentication/bad_request'
 import ConfigurationWrongCredentials from './configuration/wrong_credentials'
 import ConfigurationWrongFormat from './configuration/wrong_format'
 import MissingMandatoryParameter from './api/missing_mandatory_parameter'
+import WrongDatetimes from './api/wrong_datetimes'
 
 export {
   AuthenticationBadRequest,
   ConfigurationWrongFormat,
   ConfigurationWrongCredentials,
-  MissingMandatoryParameter
+  MissingMandatoryParameter,
+  WrongDatetimes
 }
