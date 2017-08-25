8e9b25050fa5c9df5a2098752d77f440929e6791
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-25 17:24:01 +0200

    chore(test/appointments/cancel.js) cancel appointment method tested

--- test/appointments/cancel.js
+++ test/appointments/cancel.js
@@ -1,8 +1,8 @@
 import test from 'ava'
-import sdk from '../../src/lib'
+import sdk from '../../src/lib/index'
 import SDK from '../../src/class/sdk'
 import mock from 'fetch-mock'
-import {MissingMandatoryParameter, BookingNotFound} from '../../src/class/exceptions'
+import {MissingMandatoryParameter, BookingNotFound} from '../../src/class/exceptions/index'
 
 let instance = {}
 
@@ -12,60 +12,41 @@ test.before(t => {
   t.is(instance instanceof SDK, true)
 })
 
-
 test('Cancel appointment', t => {
-  mock.restore()
-  mock.post(instance.url + instance.routes.cancelAppointment, {"errorCode": null})
-
-  return instance.cancelAppointment({
-    "siteCode": "ABC",
-    "bookingId": "1935472128"
+  return new Promise((resolve) => {
+    mock.restore()
+    mock.post(instance.url + instance.routes.cancelAppointment, {"errorCode": null})
+    return instance.cancelAppointment({"siteCode": "ABC", "bookingId": "1935472128"})
+      .then((response) => {
+        t.deepEqual(response, {"errorCode": null})
+        return resolve()
+      })
   })
-    .then((response) => t.deepEqual(response, {"errorCode": null}))
-})
-
-test('Delete appointment throws exception when missing mandatory request parameter', t => {
-  mock.restore()
-  mock.post(instance.url + instance.routes.cancelAppointment,
-    {
+    .then(() => { // Delete appointment throws exception when missing mandatory request parameter
+      mock.restore()
+      mock.post(instance.url + instance.routes.cancelAppointment, {
         "errorCode": "MISSING_MANDATORY_PARAMETER",
         "errorMessage": "Missing mandatory parameter : bookingId"
-    }
-  )
+      })
 
-  return instance.cancelAppointment({
-    "siteCode": "ABC"
-  })
-    .catch(e => {
-      t.is(e instanceof MissingMandatoryParameter, true)
-      t.is(e.name, `MissingMandatoryParameter`)
-      t.is(e.message, `Missing mandatoy parameter`)
-
+      return instance.cancelAppointment({"siteCode": "ABC"})
+        .catch(e => {
+          t.is(e instanceof MissingMandatoryParameter, true)
+          t.is(e.name, `MissingMandatoryParameter`)
+          t.is(e.message, `Parameter(s) ["bookingId"] missing`)
+        })
     })
-
-})
-
-test('Delete appointment not existing', t => {
-  mock.restore()
-  mock.post(instance.url + instance.routes.cancelAppointment,
-    {
+    .then(() => { // Delete appointment not existing
+      mock.restore()
+      mock.post(instance.url + instance.routes.cancelAppointment, {
         "errorCode": "BOOKING_NOT_FOUND",
         "errorMessage": "Unknown bookingId"
-    }
-  )
-
-  return instance.cancelAppointment({
-    "siteCode": "c1",
-    "bookingId": "20"
+      })
+      return instance.cancelAppointment({"siteCode": "c1", "bookingId": "20"})
+        .catch(e => {
+          t.is(e instanceof BookingNotFound, true)
+          t.is(e.name, `BookingNotFound`)
+          t.is(e.message, `Unknown bookingId`)
+        })
     })
-    .catch(e => {
-      t.is(e instanceof BookingNotFound, true)
-      t.is(e.name, `BookingNotFound`)
-      t.is(e.message, `Reservation to  delete is not found`)
-
-    })
-
 })
-
-
-// TODO: Tests are missing, you don't test your throwing errors
